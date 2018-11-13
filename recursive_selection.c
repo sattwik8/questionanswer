6a69e1a86c5dd07da7e6ec9b84bba9fb1a5a0966
baba <sushilkumardwivedi>
2018-11-13 23:54:52 +0530

    updated

--- recursive_selection.c
+++ recursive_selection.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-void recursive_selection(int array[],int size,int curr_index){
-	if(curr_index<size){
-		int extreme=curr_index;
-		for(int i=curr_index+1;i<size;i++){
-			if(array[extreme]>array[i])
+void recursive_selection(int array[],int curr_index){
+	if(curr_index>0){
+		int extreme=0;
+		for(int i=1;i<=curr_index;i++){
+			if(array[extreme]<array[i])
 				extreme=i;
 		}
-		if(extreme!=curr_index){
 		int temp=array[extreme];
 		array[extreme]=array[curr_index];
 		array[curr_index]=temp;
-		}
-		recursive_selection(array,size,curr_index+1);
+		recursive_selection(array,curr_index-1);
 	}
 }
 int main(int argc,char** argv){
 	int array[]={4,5,1,7,3,8,9};
-	recursive_selection(array,7,0);
+	recursive_selection(array,6);
 	for(int i=0;i<7;i++)
 	printf("%d ",array[i]);
 	printf("\n");
